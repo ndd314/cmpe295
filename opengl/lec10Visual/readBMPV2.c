882468830bcc245808f1108d36537dc55bfeef6b
Duy Nguyen <ndd314@gmail.com>
2014-11-04 16:56:24 -0800

    Rename file since it is identical to the one in ../plane project

--- opengl/lec10Visual/readBMPV2.c
+++ opengl/lec10Visual/readBMPV2.c
@@ -1,74 +1,74 @@
 /*********************************************************************
- *  This program was prepared for digital image processing class, the
- *  program was from the opensource with the credit to:
- *
+ *  This program was prepared for digital image processing class, the 
+ *  program was from the opensource with the credit to: 
+ *                                           
  *  readBMP.c
  *
  *  Created by Nina Amenta on Sun May 23 2004.
  *  Free to good home!
  *
- * Note(Harry 2013-8-25):
- *       1. use this program together with bmp.c for bmp image loading
- *          and display.
- *       2. compile and build instruction:
- *         gcc bmp.c readBMPV2.c -o bmp.o -lGL -lGLU -lglut
- *    be sure to have both program bmp.c and readBMP.c and
+ * Note(Harry 2013-8-25): 
+ *       1. use this program together with bmp.c for bmp image loading 
+ *          and display. 
+ *       2. compile and build instruction: 
+ *         gcc bmp.c readBMPV2.c -o bmp.o -lGL -lGLU -lglut  
+ *    be sure to have both program bmp.c and readBMP.c and   
  *    readBMP.h in the same directory.
  *       3. use this progra with pixelrw.c for bmp pixel read and write,
  *    the compile and build is the same as the above, except replace bmp.c
- *    by pixelrw.c
- *       4. readBMPV2.c assumes bmp image is 24 bit color image.
- *
+ *    by pixelrw.c 
+ *       4. readBMPV2.c assumes bmp image is 24 bit color image.       
+ *  
  ********************************************************************/
 
 #include <stdio.h>      // Header file for standard file i/o.
 #include <stdlib.h>     // Header file for malloc/free.
 #include "readBMP.h"
 
-/* Simple BMP reading code, should be adaptable to many systems. Originally from
-   Windows, ported to Linux, now works on my Mac OS system.
+/* Simple BMP reading code, should be adaptable to many systems. Originally from 
+   Windows, ported to Linux, now works on my Mac OS system.   
    NOTE!! only reads 24-bit RGB, single plane, uncompressed, unencoded
    BMP, not all BMPs. BMPs saved by xv should be fine. */
 //
-// This code was created by Jeff Molofee '99
+// This code was created by Jeff Molofee '99 
 //  (www.demonews.com/hosted/nehe)
 // Ported to Linux/GLUT by Richard Campbell '99
-// Code and comments for adaptation to big endian/little endian system
+// Code and comments for adaptation to big endian/little endian system 
 
 // Nina Amenta '04
 //
-/* Reads a long 32 bit integer; comment out one or the
-other shifting line below,
+/* Reads a long 32 bit integer; comment out one or the 
+other shifting line below, 
    whichever makes your system work right. */
 
 unsigned int endianReadInt(FILE* file) {
-unsigned char  b[4];
+unsigned char  b[4]; 
 unsigned int i;
 if ( fread( b, 1, 4, file) < 4 )
 return 0;
 
-/* LITTLE VS BIG ENDIAN LINES - comment out
+/* LITTLE VS BIG ENDIAN LINES - comment out 
 one or the other */
-i = (b[3]<<24) | (b[2]<<16) | (b[1]<<8) | b[0];
+i = (b[3]<<24) | (b[2]<<16) | (b[1]<<8) | b[0]; 
 // big endian
-//i = (b[0]<<24) | (b[1]<<16) | (b[2]<<8) |
+//i = (b[0]<<24) | (b[1]<<16) | (b[2]<<8) | 
 
 b[3]; // little endian
 return i;
 }
 
-/* Reads a 16 bit integer; comment out one or the other
-shifting line below,
+/* Reads a 16 bit integer; comment out one or the other 
+shifting line below, 
    whichever makes your system work right. */
 unsigned short int endianReadShort(FILE* file) {
 //11
 
-unsigned char  b[2];
+unsigned char  b[2]; 
 unsigned short s;
 if ( fread( b, 1, 2, file) < 2 )
 return 0;
 
-/* LITTLE VS BIG ENDIAN LINES - comment out
+/* LITTLE VS BIG ENDIAN LINES - comment out 
 one or the other */
 
 s = (b[1]<<8) | b[0]; // big endian
@@ -76,14 +76,14 @@ s = (b[1]<<8) | b[0]; // big endian
 return s;
 }
 
-// quick and dirty bitmap loader...for 24 bit bitmaps with 1 plane only.
+// quick and dirty bitmap loader...for 24 bit bitmaps with 1 plane only.  
 // See http://www.dcs.ed.ac.uk/~mxr/gfx/2d/BMP.txt for more info.
 
 int ImageLoad(char *filename, Image *image) {
 FILE *file;
-unsigned long size;                 // size of the image in bytes
+unsigned long size;                 // size of the image in bytes  
 unsigned long i;                    // standard counter.
-unsigned short int planes;          // number of planes in image (must be 1)
+unsigned short int planes;          // number of planes in image (must be 1) 
 unsigned short int bpp;             // number of bits per pixel (must be 24)
 char temp;                          // temporary color storage for bgr-rgb conversion.
 
@@ -99,16 +99,16 @@ fseek(file, 18, SEEK_CUR);
 
 // read the width
 if (!(image->sizeX = endianReadInt(file))) {
-printf("Error reading width from %s.\n",
+printf("Error reading width from %s.\n", 
 filename);
 return 0;
 }
 
 printf("Width of %s: %lu\n", filename, image->sizeX);
-// read the height
+// read the height 
 
 if (!(image->sizeY = endianReadInt(file))) {
-printf("Error reading height from %s.\n",
+printf("Error reading height from %s.\n", 
 filename);
 return 0;
 }
@@ -118,12 +118,12 @@ printf("Height of %s: %lu\n", filename, image->sizeY);
 size = image->sizeX * image->sizeY * 3;
 // read the planes
 if (!(planes=endianReadShort(file))) {
-printf("Error reading planes from %s.\n",
+printf("Error reading planes from %s.\n", 
 filename);
 return 0;
 }
 if (planes != 1) {
-printf("Planes from %s is not 1: %u\n",
+printf("Planes from %s is not 1: %u\n", 
 filename, planes);
 return 0;
 }
@@ -131,12 +131,12 @@ return 0;
 // read the bits per pixel
 
 if (!(bpp = endianReadShort(file))) {
-printf("Error reading bpp from %s.\n",
+printf("Error reading bpp from %s.\n", 
 filename);
 return 0;
 }
 if (bpp != 24) {
-printf("Bpp from %s is not 24: %u\n", filename,
+printf("Bpp from %s is not 24: %u\n", filename, 
 bpp);
 return 0;
 }
@@ -145,7 +145,7 @@ return 0;
 
 fseek(file, 24, SEEK_CUR);
 
-// read the data.
+// read the data. 
 
 image->data = (char *) malloc(size);
 if (image->data == NULL) {
@@ -153,7 +153,7 @@ printf("Error allocating memory for color-corrected image data");
 return 0;
 }
 if ((i = fread(image->data, size, 1, file)) != 1) {
-printf("Error reading image data from %s.\n",
+printf("Error reading image data from %s.\n", 
 filename);
 return 0;
 }
